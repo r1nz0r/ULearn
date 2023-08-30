9f9d003a8c8992a69889c0eaa28b356553aacd11
Tatiana Zaharova <33631903+r1nz0r@users.noreply.github.com>
2023-08-30 15:31:30 +0300

    Final Arr2.cs

--- Arr2.cs
+++ Arr2.cs
@@ -9,22 +9,30 @@ namespace Arrays {
         private static void Main(string[] args) {
             Random random = new Random();
             int maxValue = 9;
-            var array1 = GenerateRandomSortedArray(random, 5, maxValue);
-            var array2 = GenerateRandomSortedArray(random, 8, maxValue);
+            var array1 = GenerateRandomSorted(random, 5, maxValue);
+            var array2 = GenerateRandomSorted(random, 8, maxValue);
 
             Console.Write("Array 1:\t");
-            PrintArray(array1);
+            Print(array1);
             Console.Write("Array 2:\t");
-            PrintArray(array2);
+            Print(array2);
 
             Console.WriteLine();
+            Console.Write("Concat:\t\t");
+            Print(Concat(array1, array2));
             Console.Write("Union:\t\t");
-            PrintArray(UnionArrays(array1, array2));
+            Print(Union(array1, array2));
             Console.Write("Intersect:\t");
-            PrintArray(IntersectArrays(array1, array2));
+            Print(Intersect(array1, array2));
+            Console.Write("Except (arr1 - arr2):\t");
+            Print(Except(array1, array2));
+            Console.Write("Except (arr2 - arr1):\t");
+            Print(Except(array2, array1));
+
+            Console.ReadKey();
         }
 
-        private static int[] GenerateRandomSortedArray(Random random, int length, int maxValue) {
+        private static int[] GenerateRandomSorted(Random random, int length, int maxValue) {
             maxValue += 1;
 
             var array = new int[length];
@@ -36,26 +44,22 @@ namespace Arrays {
             return array;
         }
 
-        private static void PrintArray(int[] array) {
+        private static void Print(int[] array) {
             foreach (var item in array)
                 Console.Write(item + " ");
 
             Console.WriteLine();
         }
 
-        private static int[] IntersectArrays(int[] arr1, int[] arr2) {
+        private static int[] Intersect(int[] arr1, int[] arr2) {
             var newArray = new int[0];
+
             for (int i = 0; i < arr1.Length; ++i) {
                 for (int j = 0; j < arr2.Length; ++j) {
                     int indexOfElement = Array.IndexOf(newArray, arr2[j]);
 
                     if (arr2[j] == arr1[i] && indexOfElement == -1) {
-                        var tempArray = new int[newArray.Length + 1];
-
-                        for (int k = 0; k < newArray.Length; ++k)
-                            tempArray[k] = newArray[k];
-
-                        newArray = tempArray;
+                        newArray = Resize(newArray, newArray.Length + 1);
                         newArray[newArray.Length - 1] = arr1[i];
                     }
                 }
@@ -64,20 +68,74 @@ namespace Arrays {
             return newArray;
         }
 
-        private static int[] UnionArrays(int[] arr1, int[] arr2) {
+        private static int[] Resize(int[] arr, int newSize) {
+            var tempArray = new int[newSize];
+            int minLength = arr.Length <= tempArray.Length ? arr.Length : tempArray.Length;
+
+            for (int k = 0; k < minLength; ++k)
+                tempArray[k] = arr[k];
+
+            arr = tempArray;
+            return arr;
+        }
+
+        private static int[] Concat(int[] arr1, int[] arr2) {
             int totalArrayLength = arr1.Length + arr2.Length;
             var newArray = new int[totalArrayLength];
 
-            CopyArray(arr1, newArray);
-            CopyArray(arr2, newArray, arr1.Length);
+            Copy(arr1, newArray);
+            Copy(arr2, newArray, arr1.Length);
 
             Array.Sort(newArray);
             return newArray;
         }
 
-        private static void CopyArray(int[] oldArr, int[] newArray, int indexOffset = 0) {
+        private static int[] MakeUnique(int[] arr) {
+            int lastUniqueIndex = 0;
+            int currentIndex = 0;
+
+            if (arr.Length == 0)
+                return new int[0];
+
+            while (++currentIndex < arr.Length) {
+                if (arr[lastUniqueIndex] != arr[currentIndex])
+                    arr[++lastUniqueIndex] = arr[currentIndex];
+            }
+
+            int newSize = ++lastUniqueIndex;
+            var uniqueArr = Resize(arr, newSize);
+
+            return uniqueArr;
+        }
+
+        private static void Copy(int[] oldArr, int[] newArray, int indexOffset = 0) {
             for (int i = 0; i < oldArr.Length; ++i)
                 newArray[i + indexOffset] = oldArr[i];
+        }
+
+        private static int[] Union(int[] arr1, int[] arr2) => MakeUnique(Concat(arr1, arr2));
+
+        private static int[] Except(int[] arr1, int[] arr2) {
+            var newArr = new int[arr1.Length];
+            int indexArr1 = 0;
+            int indexArr2 = 0;
+            int indexNewArr = 0;
+
+            while (indexArr1 < arr1.Length) {
+
+                while (indexArr2 < arr2.Length && arr2[indexArr2] < arr1[indexArr1])
+                    ++indexArr2;
+
+                if (indexArr2 < arr2.Length && arr1[indexArr1] == arr2[indexArr2])
+                    ++indexArr2;
+
+                else if (indexArr2 == arr2.Length || arr2[indexArr2] > arr1[indexArr1])
+                    newArr[indexNewArr++] = arr1[indexArr1];
+
+                ++indexArr1;
+            }
+
+            return Resize(newArr, indexNewArr);
         }
     }
 }
